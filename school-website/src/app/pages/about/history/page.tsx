5465e821524e6e85e940b99c1301a5394b085380
thaKen-Kong <110529777+thaKen-Kong@users.noreply.github.com>
2026-04-11 21:10:16 +0800

    page

--- school-website/src/app/pages/about/history/page.tsx
+++ school-website/src/app/pages/about/history/page.tsx
@@ -1,12 +1,12 @@
 import { PageContainer } from "@/components/Container";
+import { Label } from "@/components/items/Label";
 
 
 export default function History() {
     return (
         <>
             <PageContainer >
-
-                
+                <Label text="HISTORY" />
             </PageContainer>
         </>
     )
